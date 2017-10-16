0bcdc2d8b9954f1263aa09513ede748465550e46
Matthew Stowe <matt@twentysix22.com>
2017-10-16 19:17:45 +0100

    scaffold out user profile changes

--- public/js/app.js
+++ public/js/app.js
@@ -54510,30 +54510,78 @@ var angular = __webpack_require__("./node_modules/angular/index.js");
 
 angular.module('app.profile').controller('ProfileController', ProfileController);
 
-ProfileController.$inject = ['$scope', '$state', 'AuthService'];
+ProfileController.$inject = ['$scope', '$state', 'AuthService', 'UserService'];
 
 /* @ngInject */
-function ProfileController($scope, $state, AuthService) {
-    var vm = this;
+function ProfileController($scope, $state, AuthService, UserService) {
+  var vm = this;
 
-    vm.getUser = getUser;
+  vm.getUser = getUser;
 
-    /////////////////////////////////////////////////
-    activate();
+  vm.localUser = {};
+  vm.areDetailsValid = areDetailsValid;
+  vm.arePasswordDetailsValid = arePasswordDetailsValid;
+  vm.submitDetails = submitDetails;
+  vm.submitPasswordChange = submitPasswordChange;
 
-    function activate() {}
+  /////////////////////////////////////////////////
+  activate();
 
+  function activate() {
+    vm.localUser = getUser();
     //$scope.$watch( AuthService.currentUser, function ( currentUser ) {
     //console.log(currentUser);
     //});
+  }
+
+  /////////////////////////////////////////////////
 
 
-    /////////////////////////////////////////////////
+  function getUser() {
+    return AuthService.currentUser();
+  }
 
-
-    function getUser() {
-        return AuthService.currentUser();
+  function areDetailsValid() {
+    var valid = true;
+    if (vm.localUser.firstname == null) {
+      valid = false;
     }
+    if (vm.localUser.lastname == null) {
+      valid = false;
+    }
+    if (vm.localUser.email == null) {
+      valid = false;
+    }
+    return valid;
+  }
+
+  function arePasswordDetailsValid() {
+    var valid = true;
+    if (vm.localUser.password == null) {
+      valid = false;
+    }
+    if (vm.localUser.password_confirm == null) {
+      valid = false;
+    }
+    if (vm.localUser.password != vm.localUser.password_confirm) {
+      valid = false;
+    }
+    return valid;
+  }
+
+  //Submit details to be saved
+  //
+  //
+  function submitDetails() {
+    console.log(vm.localUser);
+  }
+
+  //Submit details to be saved
+  //
+  //
+  function submitPasswordChange() {
+    console.log(vm.localUser);
+  }
 }
 
 /***/ }),
--- resources/assets/js/profile/profile.controller.js
+++ resources/assets/js/profile/profile.controller.js
@@ -7,20 +7,25 @@
         .module('app.profile')
         .controller('ProfileController', ProfileController);
 
-    ProfileController.$inject = ['$scope','$state','AuthService'];
+    ProfileController.$inject = ['$scope','$state','AuthService','UserService'];
 
     /* @ngInject */
-    function ProfileController($scope, $state, AuthService) {
+    function ProfileController($scope, $state, AuthService, UserService) {
         var vm = this;
 
         vm.getUser = getUser;
 
+        vm.localUser = {};
+        vm.areDetailsValid = areDetailsValid;
+        vm.arePasswordDetailsValid = arePasswordDetailsValid;
+        vm.submitDetails = submitDetails;
+        vm.submitPasswordChange = submitPasswordChange;
 
         /////////////////////////////////////////////////
         activate();
 
         function activate() {
-
+            vm.localUser = getUser();
             //$scope.$watch( AuthService.currentUser, function ( currentUser ) {
               //console.log(currentUser);
             //});
@@ -31,6 +36,41 @@
 
         function getUser() {
           return AuthService.currentUser();
+        }
+
+
+        function areDetailsValid() {
+          var valid = true;
+          if (vm.localUser.firstname == null) { valid = false; }
+          if (vm.localUser.lastname == null) { valid = false; }
+          if (vm.localUser.email == null) { valid = false; }
+          return valid;
+        }
+
+
+        function arePasswordDetailsValid() {
+          var valid = true;
+          if (vm.localUser.password == null) { valid = false; }
+          if (vm.localUser.password_confirm == null) { valid = false; }
+          if (vm.localUser.password != vm.localUser.password_confirm) { valid = false; }
+          return valid;
+        }
+
+
+        //Submit details to be saved
+        //
+        //
+        function submitDetails() {
+          console.log(vm.localUser);
+        }
+
+
+
+        //Submit details to be saved
+        //
+        //
+        function submitPasswordChange() {
+          console.log(vm.localUser);
         }
 
 
