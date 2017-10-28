5f6776282d9728de073fe0753b04aa1004cb2296
Matthew Stowe <matt@twentysix22.com>
2017-10-28 16:24:19 +0100

    started geopicker

---
+++ resources/assets/js/blocks/mapping/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var angular = require('angular');
+
+require('angular-simple-logger'); // requirement for angular-google-maps
+require('angular-google-maps');
+
+angular.module('blocks.mapping', [
+  'uiGmapgoogle-maps'
+]);
+
+require('./config');
+require('./locationpicker.directive');
